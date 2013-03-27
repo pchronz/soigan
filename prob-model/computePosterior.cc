5c3a472e04eb7c838eca1762d140672bb9560732
Peter Chronz <peter.chronz@gwdg.de>
2013-03-27 01:18:11 +0100

    computePosterior with OCT nearly finished

--- prob-model/computePosterior.cc
+++ prob-model/computePosterior.cc
@@ -3,6 +3,15 @@
 #include "dec2oneOfK.cc"
 #include "base2decpure.cc"
 
+// XXX ad-hoc implementation, consider implementing according to 
+// NIST Engineering Statistics Handbook 6.5.4.2
+// http://www.itl.nist.gov/div898/handbook/pmc/section5/pmc542.htm
+double mvnpdf(Matrix x, Matrix mu, Matrix Sigma) {
+  double p = 0.0;
+  // TODO implement 
+  return p;
+}
+
 DEFUN_DLD (computePosterior, args, nargout, "") {
   // args
   NDArray mus = args(0).array_value();
@@ -16,7 +25,7 @@ DEFUN_DLD (computePosterior, args, nargout, "") {
   int I = X.dims()(1);
   int N = X.dims()(2);
   // compute
-  long int KtoI = pow(K, I);
+  long int KtoI = std::pow(K, I);
   Matrix p_Z(KtoI, N);
   p_Z.fill(0.0);
   for(int n = 0; n < N; n++) {
@@ -26,8 +35,8 @@ DEFUN_DLD (computePosterior, args, nargout, "") {
       dec2_args(1) = K;
       dec2_args(2) = I;
       octave_value_list dec2ret = dec2oneOfKpure(dec2_args);
-      Matrix Z_n = dec2ret(0).array_value();
-      Matrix z = dec2ret(1).array_value();
+      boolMatrix Z_n = dec2ret(0).bool_array_value();
+      charMatrix z = dec2ret(1).char_array_value();
       // select the right mus
       Matrix mus_l(D, I);
       mus_l.fill(0.0);
@@ -44,9 +53,60 @@ DEFUN_DLD (computePosterior, args, nargout, "") {
         }
       }
       // select the right Sigmas
+      dim_vector Sigmas_l_dim = dim_vector::alloc(3);
+      Sigmas_l_dim(0) = D;
+      Sigmas_l_dim(1) = D;
+      Sigmas_l_dim(2) = I;
+      NDArray Sigmas_l(Sigmas_l_dim);
+      Sigmas_l.fill(0.0);
+      for(int i = 0; i < I; i++) {
+        for(int d1 = 0; d1 < D; d1++) {
+          for(int d2 = 0; d2 < D; d2++) {
+            Matrix Sigmas_idx(1, 4);
+            Sigmas_idx(0, 0) = d1;
+            Sigmas_idx(0, 1) = d2;
+            Sigmas_idx(0, 2) = z_idx(i, 0);
+            Sigmas_idx(0, 3) = i;
+            Sigmas_l(d1, d2, i) = Sigmas(Sigmas_idx);
+          }
+        }
+      }
+      // select the right pis
+      Matrix pi_l(1, I);
+      pi_l.fill(0.0);
+      for(int i = 0; i < I; i++) {
+        pi_l(0, i) = pi(z_idx(i, 0), i);
+      }
+      // compute the posterior for the current state and observation
+      p_Z(l, n) = std::pow(rho(l, 0), d(n, 0)) * std::pow((1 - rho(l, 0)), (1 - d(n, 0)));
+      for(int i = 0; i < I; i++) {
+        Matrix x_n_i(D, 1);
+        for(int d = 0; d < D; d++) {
+          Matrix x_idx = Matrix(1, 3);
+          x_idx(0, 0) = d;
+          x_idx(0, 1) = i;
+          x_idx(0, 2) = n;
+          x_n_i(d, 0) = X(x_idx);
+        }
+        Matrix mu_l_i = mus_l.column(i);
+        Matrix Sigma_l_i(D, D);
+        for(int d1 = 0; d1 < D; d1++) {
+          for(int d2 = 0; d2 < D; d2++) {
+            Matrix Sigmas_idx(1, 3);
+            Sigmas_idx(0, 0) = d1;
+            Sigmas_idx(0, 1) = d2;
+            Sigmas_idx(0, 2) = i;
+            Sigma_l_i(d1, d2) = Sigmas_l(Sigmas_idx);
+          }
+        }
+        double p_x_n_i = mvnpdf(x_n_i, mu_l_i, Sigma_l_i);
+        p_Z(l, n) *= pi_l(0, i) * p_x_n_i;
+      }
     }
   }
   // return
-  return octave_value_list();
+  octave_value_list retval(1);
+  retval(0) = p_Z;
+  return retval;
 }
 
