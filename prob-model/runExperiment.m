20cca802e89edde0fa958ccb3f8ef1b5bf5881e7
root <root@ubuntu.(none)>
2014-07-17 13:05:04 +0200

    updated data on ews

--- prob-model/runExperiment.m
+++ prob-model/runExperiment.m
@@ -16,11 +16,11 @@ Delay = [0:0];
 It = 20;
 min_K = 2;
 max_K = 2;
-min_N = 323;
-max_N = 500;
-refresh_rate = 25;
+min_N = 1;
+max_N = 5000;
+refresh_rate = 50;
 
-global deter = true;
+global deter = false;
 X = 0;
 d = 0;
 if(deter)
