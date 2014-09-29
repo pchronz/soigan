d31201a8c878c86c0e70e9ad35b594f915b2a8b6
Peter Chronz <peter.chronz@gwdg.de>
2014-09-29 19:36:22 +0200

    updated results.

--- prob-model/runParallelExperiment.m
+++ prob-model/runParallelExperiment.m
@@ -15,10 +15,10 @@ function runParallelExperiment(X, d, min_K, max_K, S = 10)
   baseline_correctness_parallel = zeros(max_K, 2, S*(N - floor(N/S)));
   baseline_training_parallel = zeros(max_K, S);
   baseline_prediction_parallel = zeros(max_K, S);
-
-  prob_model_correctness_parallel = zeros(max_K, N);
-  prob_model_training_parallel = zeros(max_K, S);
-  prob_model_prediction_parallel = zeros(max_K, S);
+  % K X (true value, predicted value) X predictions
+  mixture_correctness_parallel = zeros(max_K, 2, S*(N - floor(N/S)));
+  mixture_training_parallel = zeros(max_K, S);
+  mixture_prediction_parallel = zeros(max_K, S);
 
   % Verify that we have a value for each class in the data set.
   first_neg = min([1:N](d == 0));
@@ -60,34 +60,23 @@ function runParallelExperiment(X, d, min_K, max_K, S = 10)
     baseline_prediction_parallel(:, s) = t_pred;
     baseline_correctness_parallel(:, :, test_idx:test_idx + length(d_test) - 1) = correctness;
 
-    for K = min_K:max_K
-      %disp('Multi-mixture model training --- parallel')
-      %tic()
-      %[mus, Sigmas, rho, pi] = learnExactIndependent(K, X_tr, d_tr, 30);
-      %elapsed = toc()
-      %prob_model_training_parallel(K, n + 1) = elapsed;
-      %disp('Multi-mixture model prediction --- parallel')
-      %tic()
-      %[p_0, p_1] = predictExactIndependent(X_red(:, :, n + 1), mus, Sigmas, rho, pi);
-      %elapsed = toc()
-      %prob_model_prediction_parallel(K, n + 1) = elapsed;
-      %prob_model_correctness_parallel(K, n + 1) = double((p_0 < p_1) == d(n + 1));
-      %prob_model_correctness_parallel(K, 1:n + 1);
-    endfor
+    % Mixture Model
+    disp('Mixture Model')
+    [t_train, t_pred, correctness] = runMixtureParallelExperiment(X_tr, d_tr, X_test, d_test, min_K, max_K);
+    mixture_training_parallel(:, s) = t_train;
+    mixture_prediction_parallel(:, s) = t_pred;
+    mixture_correctness_parallel(:, :, test_idx:test_idx + length(d_test) - 1) = correctness;
 
     % Update the position of our result pointer.
     test_idx += length(d_test);
 
     try
-      save -V7 experimentResultsParallel.mat min_K max_K S bernoulli_correctness_parallel bernoulli_training_parallel bernoulli_prediction_parallel svm_correctness_parallel svm_training_parallel svm_prediction_parallel baseline_correctness_parallel baseline_training_parallel baseline_prediction_parallel
+      save -V7 experimentResultsParallel.mat min_K max_K S bernoulli_correctness_parallel bernoulli_training_parallel bernoulli_prediction_parallel svm_correctness_parallel svm_training_parallel svm_prediction_parallel baseline_correctness_parallel baseline_training_parallel baseline_prediction_parallel mixture_correctness_parallel mixture_training_parallel mixture_prediction_parallel
       save -V7 experimentResultsParallelRelevantServices.mat services dims
       disp('The parallel results have been saved')
     catch
       error(last_error())
     end_try_catch
-
-    %  endfor
-    %endfor
 
     % Output the results
     bernoulli_correctness_parallel(:, 1:test_idx - 1)
@@ -99,18 +88,19 @@ function runParallelExperiment(X, d, min_K, max_K, S = 10)
     baseline_correctness_parallel(:, :, 1:test_idx - 1)
     baseline_training_parallel(1, s)
     baseline_prediction_parallel(1, s)
-    %prob_model_correctness_parallel
-    %prob_model_training_parallel
-    %prob_model_prediction_parallel
+    mixture_correctness_parallel(:, :, 1:test_idx - 1)
+    mixture_training_parallel(1, s)
+    mixture_prediction_parallel(1, s)
   endfor
   % Delete all unused entries in the results.
   bernoulli_correctness_parallel(:, test_idx:end) = [];
   svm_correctness_parallel(:, test_idx:end) = [];
   baseline_correctness_parallel(:, :, test_idx:end) = [];
+  mixture_correctness_parallel(:, :, test_idx:end) = [];
 
   % Final, trimmed save
   try
-    save -V7 experimentResultsParallel.mat min_K max_K S bernoulli_correctness_parallel bernoulli_training_parallel bernoulli_prediction_parallel svm_correctness_parallel svm_training_parallel svm_prediction_parallel baseline_correctness_parallel baseline_training_parallel baseline_prediction_parallel
+    save -V7 experimentResultsParallel.mat min_K max_K S bernoulli_correctness_parallel bernoulli_training_parallel bernoulli_prediction_parallel svm_correctness_parallel svm_training_parallel svm_prediction_parallel baseline_correctness_parallel baseline_training_parallel baseline_prediction_parallel mixture_correctness_parallel mixture_training_parallel mixture_prediction_parallel
     save -V7 experimentResultsParallelRelevantServices.mat services dims
     disp('The parallel results have been saved')
   catch
@@ -118,7 +108,7 @@ function runParallelExperiment(X, d, min_K, max_K, S = 10)
   end_try_catch
 endfunction
 
-function [t_train, t_pred, correctness] =  runBernoulliParallelExperiment(d_tr, d_test)
+function [t_train, t_pred, correctness] = runBernoulliParallelExperiment(d_tr, d_test)
   % Bernoulli (max likelihood)
   disp('Bernoulli model training --- parallel')
   tic()
@@ -135,7 +125,7 @@ function [t_train, t_pred, correctness] =  runBernoulliParallelExperiment(d_tr, 
   t_pred = toc()
 endfunction
 
-function [t_train, t_pred, correctness] =  runSvmParallelExperiment(X_tr, d_tr, X_test, d_test)
+function [t_train, t_pred, correctness] = runSvmParallelExperiment(X_tr, d_tr, X_test, d_test)
     % learn SVM
     tic()
     MODE.TYPE='rbf';
@@ -164,7 +154,7 @@ function [t_train, t_pred, correctness] =  runSvmParallelExperiment(X_tr, d_tr, 
     endif
 endfunction
 
-function [t_train, t_pred, correctness] =  runApproximateParallelExperiment(X_tr, d_tr, X_test, d_test, min_K, max_K)
+function [t_train, t_pred, correctness] = runApproximateParallelExperiment(X_tr, d_tr, X_test, d_test, min_K, max_K)
   t_train = zeros(max_K, 1);
   t_pred = zeros(max_K, 1);
   correctness = zeros(max_K, 2, length(d_test));
@@ -175,13 +165,33 @@ function [t_train, t_pred, correctness] =  runApproximateParallelExperiment(X_tr
     t_train(K) = toc();
     % predict baseline
     disp('Baseline model prediction --- parallel')
+    correctness(K, 1, :) = d_test;
     tic()
     for n = 1:length(d_test)
       [p_0, p_1] = predictBaseline(X_test(:, :, n), centers, rho_base);
       correctness(K, 2, n) = double((p_0 < p_1));
     endfor
     t_pred(K) = toc();
-    correctness(K, 1, 1:length(d_test)) = d_test;
+  endfor
+endfunction
+
+function [t_train, t_pred, correctness] = runMixtureParallelExperiment(X_tr, d_tr, X_test, d_test, min_K, max_K)
+  t_train = zeros(max_K, 1);
+  t_pred = zeros(max_K, 1);
+  correctness = zeros(max_K, 2, length(d_test));
+  for K = min_K:max_K
+    disp('Mixture model training --- parallel')
+    tic()
+    [mus, Sigmas, rho, pi] = learnExactIndependent(K, X_tr, d_tr, 30);
+    t_train(K) = toc();
+    disp('Mixture model prediction --- parallel')
+    tic()
+    correctness(K, 1, :) = d_test;
+    for n = 1:length(d_test)
+      [p_0, p_1] = predictExactIndependent(X_test(:, :, n), mus, Sigmas, rho, pi);
+      t_pred(K) = toc();
+      correctness(K, 2, n) = double(p_0 < p_1);
+    endfor
   endfor
 endfunction
 
