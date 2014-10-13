d72681200a0e6332599e1cafca8eb6fe60812ad9
root <root@ubuntu.(none)>
2014-10-13 17:04:57 +0200

    parallel results for ews

--- prob-model/runExperiment.m
+++ prob-model/runExperiment.m
@@ -15,8 +15,8 @@ It = 25;
 min_K = 2;
 max_K = 2;
 min_N = 1;
-max_N = 500;
-refresh_rate = 400;
+max_N = Inf;
+refresh_rate = 100;
 win_len = 1000;
 cross_S = 8;
 
@@ -31,14 +31,14 @@ else
   disp('Preparing data set from scratch.')
   % N, I, R, D, K
   %[X, d] = createSyntheticData(max_N, 5, 3, 3, 3);
-  [X, d] = loadEwsData(max_N);
-  %[X, d] = loadGoeGridFullData(0, max_N);
+  %[X, d] = loadEwsData(max_N);
+  [X, d] = loadGoeGridFullData(0, max_N);
   save detdata.mat X d
 endif
 
 disp(sum(d == 0))
 disp(sum(d == 1))
 
-[baseline_correctness_serial, baseline_training_serial, baseline_prediction_serial, prob_model_correctness_serial, prob_model_training_serial, prob_model_prediction_serial, svm_correctness_serial, svm_training_serial, svm_prediction_serial] = runSerialExperiment(X, d, min_K, max_K, min_N, refresh_rate, win_len, cross_S);
-%runParallelExperiment(X, d, min_K, max_K, 2);
+%[baseline_correctness_serial, baseline_training_serial, baseline_prediction_serial, prob_model_correctness_serial, prob_model_training_serial, prob_model_prediction_serial, svm_correctness_serial, svm_training_serial, svm_prediction_serial] = runSerialExperiment(X, d, min_K, max_K, min_N, refresh_rate, win_len, cross_S);
+runParallelExperiment(X, d, min_K, max_K, 8);
 
